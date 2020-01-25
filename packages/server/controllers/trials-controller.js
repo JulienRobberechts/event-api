e6a7c3003400594844d98f8774b3e7aa7f9f2cd9
JulienRobberechts <jrobberechts@findmypast.com>
2020-01-25 16:36:07 +0000

    :sparkles: getOngoingTrialsBySponsor implementation

--- packages/server/controllers/trials-controller.js
+++ packages/server/controllers/trials-controller.js
@@ -6,7 +6,30 @@ async function getOngoingTrialsBySponsor(sponsorName) {
     `trials-controller.getOngoingTrialsBySponsor called with: sponsorName=${sponsorName}`
   );
   const allTrials = await GetAllTrials();
-  return allTrials;
+  const currentDate = Date.now();
+
+  const ongoingTrials = allTrials
+    .filter(trialIsOngoingAt(currentDate))
+    .filter(trialIsNotCanceled)
+    .filter(trialIsSponsoredBy(sponsorName))
+    .map(trialSummary);
+
+  return ongoingTrials;
 }
+
+const trialIsOngoingAt = currentDate => trial =>
+  new Date(trial.start_date) < currentDate &&
+  new Date(trial.end_date) > currentDate;
+
+const trialIsNotCanceled = trial => !trial.canceled;
+
+const trialIsSponsoredBy = sponsorName => trial => trial.sponsor == sponsorName;
+
+const trialSummary = ({ name, start_date, end_date, sponsor }) => ({
+  name,
+  start_date,
+  end_date,
+  sponsor
+});
 
 module.exports = { getOngoingTrialsBySponsor };
