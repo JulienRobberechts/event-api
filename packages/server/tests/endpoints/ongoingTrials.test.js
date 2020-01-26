967b7b552cbede2a64d0df7fa8eb13a75eea177b
JulienRobberechts <jrobberechts@findmypast.com>
2020-01-26 10:25:01 +0000

    :white_check_mark: endpoint tests

--- packages/server/tests/endpoints/ongoingTrials.test.js
+++ packages/server/tests/endpoints/ongoingTrials.test.js
@@ -1,18 +1,26 @@
-const app = require("../../app");
 const request = require("supertest");
-const debug = require("debug")("server:api:trials");
-const { mockThirdPartyApi } = require("../mock/thirdParty");
 
+const app = require("../../app");
+const { mockThirdPartyApi } = require("../mock/thirdParty");
 const expectedOngoingTrialsForSanofi = require("../data/trials-ongoingFor-Sanofi.json");
 const expectedOngoingTrialsForAstraZeneca = require("../data/trials-ongoingFor-AstraZeneca.json");
+
+const api = request(app);
 
 describe("GET /OngoingTrials", () => {
   beforeEach(() => {
     mockThirdPartyApi();
   });
   it("/Sanofi", async () => {
-    const response = await request(app).get("/OngoingTrials/Sanofi");
-    expect(response.status).toEqual(200);
+    const response = await api.get("/OngoingTrials/Sanofi").expect(200);
     expect(response.body).toEqual(expectedOngoingTrialsForSanofi);
+  });
+  it("/AstraZeneca", async () => {
+    const response = await api.get("/OngoingTrials/AstraZeneca").expect(200);
+    expect(response.body).toEqual(expectedOngoingTrialsForAstraZeneca);
+  });
+  it("/UnknownLab", async () => {
+    const response = await api.get("/OngoingTrials/UnknownLab").expect(200);
+    expect(response.body).toEqual([]);
   });
 });
