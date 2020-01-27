f42ced8ee581a9aa5f6659622287d8c42df335f4
JulienRobberechts <jrobberechts@findmypast.com>
2020-01-27 11:31:32 +0000

    :lispstick: format output

--- packages/clinato/lib/list/formating.js
+++ packages/clinato/lib/list/formating.js
@@ -3,28 +3,26 @@ const colors = require("colors");
 const formatHeader = args => {
   console.log("");
   console.log(" Trials list ");
-  console.log("------------------");
-  console.log(colors.gray(" Filters:"));
-  console.log(" %s: %s", colors.gray("ongoing"), colors.bold("Yes"));
-  console.log(
-    " %s: %s",
-    colors.gray("country"),
-    colors.bold(args.country ? args.country : "All")
-  );
+  // console.log("------------------");
+
+  const country = colors.bold(args.country ? args.country : "All");
+  console.log(`${colors.gray(` Filters : ongoing: Yes, country: ${country}`)}`);
+
+  // console.log(colors.gray(" Filters"));
+  // console.log(" ongoing: %s", colors.bold("Yes"));
+  // console.log(" country: %s", colors.bold(args.country ? args.country : "All"));
   console.log("------------------");
 };
 
 const formatBody = trials => {
   console.log(colors.gray(` ${trials.length} trial(s) found`));
-  // console.log("");
+  console.log("");
   trials.forEach(trial => {
-    console.log(
-      " * %s %s",
-      colors.blue(trial.name),
-      colors.green(`(${trial.sponsor})`)
-    );
+    const name = colors.cyan(trial.name);
+    const sponsor = colors.green("(" + trial.sponsor + ")");
+    const period = colors.blue(`[${trial.start_date} / ${trial.end_date}]`);
+    console.log(` * ${name} ${sponsor} ${period}`);
   });
-  console.log("------------------");
 };
 
 module.exports = { formatHeader, formatBody };
