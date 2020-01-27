e40ddf33831b614812c28d78067222250db59d5d
JulienRobberechts <jrobberechts@findmypast.com>
2020-01-27 11:11:49 +0000

    :lipstick: format cli

--- packages/clinato/lib/list/formating.js
+++ packages/clinato/lib/list/formating.js
@@ -4,17 +4,25 @@ const formatHeader = args => {
   console.log("");
   console.log(" Trials list ");
   console.log("------------------");
-  console.log(colors.gray(" Filters"));
-  console.log(" ongoing: %s", colors.bold("Yes"));
-  console.log(" country: %s", colors.bold(args.country ? args.country : "All"));
+  console.log(colors.gray(" Filters:"));
+  console.log(" %s: %s", colors.gray("ongoing"), colors.bold("Yes"));
+  console.log(
+    " %s: %s",
+    colors.gray("country"),
+    colors.bold(args.country ? args.country : "All")
+  );
   console.log("------------------");
 };
 
 const formatBody = trials => {
-  console.log(` ${trials.length} trial(s) found`);
-  console.log("");
+  console.log(colors.gray(` ${trials.length} trial(s) found`));
+  // console.log("");
   trials.forEach(trial => {
-    console.log(" * %s %s", trial.name, colors.green(`(${trial.sponsor})`));
+    console.log(
+      " * %s %s",
+      colors.blue(trial.name),
+      colors.green(`(${trial.sponsor})`)
+    );
   });
   console.log("------------------");
 };
